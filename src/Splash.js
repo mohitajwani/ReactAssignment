09b1124b329f22d74e9c6e131635832567f36263
Mohit Ajwani <mohit.a@westagilelabs.com>
2018-10-14 15:39:52 +0530

    Separating Splash

---
+++ src/Splash.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Splash extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>Welcome to EMS</p>
+                        <p>This project is made by Mohit Ajwani</p>
+                    </header>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Splash;
